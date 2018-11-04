da5fbf201ffbee58c6ce8d2ba2220868cbac4410
mlek14 <lekman.1414@gmail.com>
2018-11-04 13:53:20 +0700

    init

---
+++ menu-index.php
@@ -0,0 +1,29 @@
+
+<html>
+<title>การพัฒนาระบบฐานข้อมูลการจองพื้นที่ตลาดนัดออนไลน์</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+<body>
+
+<ul>
+  <li><a href="index.php">หน้รแรก</a></li>
+  <li><a href="#news">สมัตรสมาชิก</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+	<li><a href="login.php">เข้าสู่ระบบ</a></li>
+  </li>
+</ul>
+
+</body>
+
+</html>
---
+++ nav-menu/menu-index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">Logo</a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+</nav>
+
+
+<ul>
+  <li><a href="index.php">หน้รแรก</a></li>
+  <li><a href="#news">สมัตรสมาชิก</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  <li><a href="login.php">เข้าสู่ระบบ</a></li>
+  </li>
+</ul>
+
+</body>
+</html>
