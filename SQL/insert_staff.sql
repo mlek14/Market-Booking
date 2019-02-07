8e8c5699fcc0b37981e59a3841b096bbab95d1d0
kernorasing <kernorasing@gmail.com>
2019-02-08 00:56:38 +0700

    fix delete staff

---
+++ SQL/insert_staff.sql
@@ -0,0 +1,7 @@
+insert into staffs
+(staff_roleId, staff_firstname, staff_lastname, staff_email, staff_username, staff_password, staff_phone_no)
+values
+(1, "Admin", "02", "admin02@mail.com", "admin02", "1", "0999999999"),
+(1, "Admin", "03", "admin03@mail.com", "admin03", "1", "0999999998"),
+(2, "Staff", "02", "staff02@mail.com", "staff02", "1", "0111111111"),
+(2, "Staff", "03", "staff03@mail.com", "staff03", "1", "0111111112")
--- api/staff/delete_staff.php
+++ api/staff/delete_staff.php
@@ -6,11 +6,14 @@ $db = new ConnectDB();
 if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
     $staffId = $_GET["staffId"];
-    $query = $db->query("update staffs set staff_status = 'rmv' where Id = $staffId");
-    if ($query)
+    $staffRoleId = $db->query("select staff_roleId from staffs where Id = $staffId")->fetch_assoc()["staff_roleId"];
+    if ($staffRoleId != 1)
     {
-        echo json_encode(true);
+        $query = $db->query("update staffs set staff_status = 'rmv' where Id = $staffId");
+        if ($query)
+        {
+            echo json_encode(true);
+        }
     }
-
     $db->close();
 }
